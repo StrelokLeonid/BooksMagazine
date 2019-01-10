8ea3cd8c27c233853bfde2530dfb38b26b6f03c1
Леонид <@DESKTOP-UGMELOB>
2019-01-11 00:48:36 +0500

    Successful

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +13,63 @@ namespace ConsoleApp3
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
+            my n = new my();
+            UseSqlLite uql = new UseSqlLite();
+            uql.LoadData();
+            //n.DB(@"https://proklondike.net/books.html");
+            Console.WriteLine("Test");
+            Console.ReadLine();
         }
+
+
+    }
+
+    class my
+    {
+        
+        public async Task DBR(string website)
+        {
+            A:
+
+            HttpClient http = new HttpClient();
+            var response = await http.GetByteArrayAsync(website);
+            String source = Encoding.GetEncoding("utf-8").GetString(response, 0, response.Length - 1);
+            source = WebUtility.HtmlDecode(source);
+            HtmlDocument resultat = new HtmlDocument();
+            resultat.LoadHtml(source);
+
+            List<HtmlNode> toftitle = resultat.DocumentNode.Descendants().Where
+            (x => (x.Name == "div" && x.Attributes["class"] != null &&
+            x.Attributes["class"].Value.Contains("book-list-item"))).ToList();
+
+
+            //var li = toftitle[6].Descendants("div").ToList();
+            foreach (var item in toftitle)
+            {
+                var link = item.Descendants("a").ToList()[0].GetAttributeValue("href", null);
+                var img = item.Descendants("img").ToList()[0].GetAttributeValue("src", null);
+                var title = item.Descendants("h3").ToList()[0].InnerText;
+                Console.WriteLine($"Link: {link}; IMG: {img}, TITLE: {title}"  );
+            }
+
+            for (int i = 2; i < 129; i++)
+            {
+                website = $"/books/page/{i}.html";
+                if (website != "https://proklondike.net/books.html")
+                {
+                    website = $"https://proklondike.net/books/page/{i}.html";
+                    goto A;
+                }
+            }
+
+        }
+
+        
+
+
     }
 }
---
+++ ConsoleApp3/UseSqlLite.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class UseSqlLite
+    {
+        private SQLiteConnection sql_con;
+        private SQLiteCommand sql_cmd;
+        private SQLiteDataAdapter DB;
+        private DataSet DS = new DataSet();
+        private DataTable DT = new DataTable();
+
+
+        public void SetConnection()
+        {
+            sql_con = new SQLiteConnection("Data Source=my_first_db.sqlite3;Version=3;New=False;Compress=True;");
+        }
+
+
+        public void ExecuteQuery(string txtQuery)
+        {
+            SetConnection();
+            sql_con.Open();
+            sql_cmd = sql_con.CreateCommand();
+            sql_cmd.CommandText = txtQuery;
+            sql_cmd.ExecuteNonQuery();
+            sql_con.Close();
+        }
+
+        public void LoadData()
+        {
+            SetConnection();
+            sql_con.Open();
+            sql_cmd = sql_con.CreateCommand();
+            string CommandText = "select id, desc from table1";
+            DB = new SQLiteDataAdapter(CommandText, sql_con);
+            DS.Reset();
+            DB.Fill(DS);
+            DT = DS.Tables[0];
+            Console.WriteLine(DT);
+            sql_con.Close();
+        }
+
+        public void Add(string txtDesc)
+        {
+            string txtSQLQuery = "insert into  mains (desc) values ('" + txtDesc + "')";
+            ExecuteQuery(txtSQLQuery);
+        }
+    }
+}
